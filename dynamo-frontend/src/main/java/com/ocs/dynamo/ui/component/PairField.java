c8b6284bcd3a5d8795b496c35658ab1f3b2a05a8
Michael Westergaard <michael.westergaard@opencircle.solutions>
2018-05-08 14:40:09 +0200

    Fix propagation in PairField

--- dynamo-frontend/src/main/java/com/ocs/dynamo/ui/component/PairField.java
+++ dynamo-frontend/src/main/java/com/ocs/dynamo/ui/component/PairField.java
@@ -1,6 +1,7 @@
 package com.ocs.dynamo.ui.component;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.log4j.Logger;
 
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomField;
@@ -8,11 +9,15 @@ import com.vaadin.ui.Field;
 import com.vaadin.ui.HorizontalLayout;
 
 public class PairField<L, R> extends CustomField<Pair<L, R>> {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = Logger.getLogger(PairField.class);
 
 	private Field<L> left;
 	private Field<R> right;
 	private Component middle;
-	
+
+	private boolean maskChanges = false;
+
 	public PairField(Field<L> left, Field<R> right) {
 		this(left, right, null);
 	}
@@ -24,23 +29,31 @@ public class PairField<L, R> extends CustomField<Pair<L, R>> {
 		this.left = left;
 		this.right = right;
 		this.middle = middle;
-		ValueChangeListener propagator = new EventPropagator();
-		left.addValueChangeListener(propagator);
-		right.addValueChangeListener(propagator);
+		
+		ValueChangeListener listener = e -> {
+			synchronized (PairField.this) {
+				if (!maskChanges) {
+					maskChanges = true;
+					setValue(Pair.of(left.getValue(), right.getValue()));
+					maskChanges = false;
+				}
+			}
+		};
+		left.addValueChangeListener(listener);
+		right.addValueChangeListener(listener);
+		
+		addValueChangeListener(e -> {
+			synchronized (PairField.this) {
+				if (!maskChanges) {
+					maskChanges = true;
+					left.setValue(getValue().getLeft());
+					right.setValue(getValue().getRight());
+					maskChanges = false;
+				}
+			}
+		});
 	}
-	
-	private class EventPropagator implements ValueChangeListener {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 2613566569836700937L;
 
-		@Override
-		public void valueChange(com.vaadin.data.Property.ValueChangeEvent event) {
-			PairField.this.fireValueChange(false);
-		}
-	}
- 
 	/**
 	 * 
 	 */
@@ -58,22 +71,6 @@ public class PairField<L, R> extends CustomField<Pair<L, R>> {
 		root.addComponent(right);
 
 		return root;
-	}
-
-	@Override
-	protected void setInternalValue(Pair<L, R> newFieldValue) {
-		if (newFieldValue != null) {
-			left.setValue(newFieldValue.getLeft());
-			right.setValue(newFieldValue.getRight());
-		} else {
-			left.setValue(null);
-			right.setValue(null);
-		}
-	}
-
-	@Override
-	protected Pair<L, R> getInternalValue() {
-		return Pair.of(left.getValue(), right.getValue());
 	}
 
 	@SuppressWarnings("unchecked")
