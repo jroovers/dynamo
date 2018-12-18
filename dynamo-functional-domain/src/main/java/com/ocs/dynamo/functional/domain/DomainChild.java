367b4f9db399a222ee6631918ddf603259eab4db
Jeroen Weijers <jeroenweijers@gmail.com>
2018-12-18 09:54:18 +0100

    Fix hibernate warning, inheritance and lazy loading are difficult with generics...

--- dynamo-functional-domain/src/main/java/com/ocs/dynamo/functional/domain/DomainChild.java
+++ dynamo-functional-domain/src/main/java/com/ocs/dynamo/functional/domain/DomainChild.java
@@ -13,13 +13,12 @@
  */
 package com.ocs.dynamo.functional.domain;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 /**
  * The persistent class for the domain entity that manages hierarchical reference information.
@@ -34,7 +33,10 @@ public abstract class DomainChild<C extends DomainChild<C, P>, P extends DomainP
 	private static final long serialVersionUID = 2615942460028599211L;
 
 	@NotNull
-	@ManyToOne(fetch = FetchType.LAZY, targetEntity = DomainParent.class)
+	//Eager fetch to prevent hibernate warnings on proxy narrowing, due to the given generic targetEntity
+	//potentially dangerous proxy classes are created (extending DomainParent and not the actual instance).
+	@ManyToOne(fetch = FetchType.EAGER,
+			   targetEntity = DomainParent.class)
 	@JoinColumn(name = "parent")
 	private P parent;
 
