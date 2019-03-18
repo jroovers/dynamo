49225ec66a79a41d1ec2598de6031384acefe3d9
wittekip <bas.rutten@opencirclesolutions.nl>
2019-03-18 10:47:33 +0100

    whatever

--- dynamo-frontend/src/main/java/com/ocs/dynamo/ui/composite/layout/HorizontalDisplayLayout.java
+++ dynamo-frontend/src/main/java/com/ocs/dynamo/ui/composite/layout/HorizontalDisplayLayout.java
@@ -15,12 +15,16 @@ package com.ocs.dynamo.ui.composite.layout;
 
 import java.io.Serializable;
 
+import com.jarektoro.responsivelayout.ResponsiveLayout;
+import com.jarektoro.responsivelayout.ResponsiveRow;
+import com.ocs.dynamo.constants.DynamoConstants;
 import com.ocs.dynamo.domain.AbstractEntity;
 import com.ocs.dynamo.domain.model.AttributeModel;
 import com.ocs.dynamo.domain.model.AttributeType;
 import com.ocs.dynamo.domain.model.EntityModel;
 import com.ocs.dynamo.service.BaseService;
 import com.ocs.dynamo.ui.component.DefaultHorizontalLayout;
+import com.ocs.dynamo.ui.component.ResponsiveUtil;
 import com.vaadin.ui.HorizontalLayout;
 
 /**
@@ -28,49 +32,44 @@ import com.vaadin.ui.HorizontalLayout;
  * header bar)
  * 
  * @author bas.rutten
- * @param <ID>
- *            the type of the primary key of the entity
- * @param <T>
- *            the type of the entity being displayed
+ * @param <ID> the type of the primary key of the entity
+ * @param <T> the type of the entity being displayed
  */
-public class HorizontalDisplayLayout<ID extends Serializable, T extends AbstractEntity<ID>>
-		extends BaseServiceCustomComponent<ID, T> {
+public class HorizontalDisplayLayout<ID extends Serializable, T extends AbstractEntity<ID>> extends BaseServiceCustomComponent<ID, T> {
 
-	private static final long serialVersionUID = -2610435729199505546L;
+    private static final long serialVersionUID = -2610435729199505546L;
 
-	private T entity;
+    private T entity;
 
-	/**
-	 * Constructor
-	 * 
-	 * @param service
-	 *            the service used to query the database
-	 * @param entityModel
-	 *            the entity model of the entity to display
-	 * @param entity
-	 *            the entity to display
-	 * @param formOptions
-	 *            the form options that govern how the layout is displayed
-	 */
-	public HorizontalDisplayLayout(BaseService<ID, T> service, EntityModel<T> entityModel, T entity) {
-		super(service, entityModel, new FormOptions());
-		this.entity = entity;
-	}
+    /**
+     * Constructor
+     * 
+     * @param service     the service used to query the database
+     * @param entityModel the entity model of the entity to display
+     * @param entity      the entity to display
+     * @param formOptions the form options that govern how the layout is displayed
+     */
+    public HorizontalDisplayLayout(BaseService<ID, T> service, EntityModel<T> entityModel, T entity) {
+        super(service, entityModel, new FormOptions());
+        this.entity = entity;
+    }
 
-	@Override
-	public void attach() {
-		super.attach();
-		build();
-	}
+    @Override
+    public void attach() {
+        super.attach();
+        build();
+    }
 
-	@Override
-	public void build() {
-		HorizontalLayout layout = new DefaultHorizontalLayout(false, true, true);
-		for (AttributeModel attributeModel : getEntityModel().getAttributeModels()) {
-			if (attributeModel.isVisible() && AttributeType.BASIC.equals(attributeModel.getAttributeType())) {
-				layout.addComponent(constructLabel(entity, attributeModel));
-			}
-		}
-		setCompositionRoot(layout);
-	}
+    @Override
+    public void build() {
+        ResponsiveLayout layout = new ResponsiveLayout();
+        ResponsiveRow row = layout.addRow();
+        for (AttributeModel attributeModel : getEntityModel().getAttributeModels()) {
+            if (attributeModel.isVisible() && AttributeType.BASIC.equals(attributeModel.getAttributeType())) {
+                row.addColumn().withDisplayRules(DynamoConstants.HALF_COLUMNS, 3, 3, 3)
+                        .withComponent(constructLabel(entity, attributeModel));
+            }
+        }
+        setCompositionRoot(layout);
+    }
 }
